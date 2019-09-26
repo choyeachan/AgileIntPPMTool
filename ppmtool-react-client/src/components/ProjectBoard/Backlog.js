e1aac6595dcedce359db5203c27d2558d5fddf7a
choyeachan <choyuichan@gmail.com>
2019-09-26 14:37:46 +0900

    branch52 - projectTask 상태별 분류 및 수정

--- ppmtool-react-client/src/components/ProjectBoard/Backlog.js
+++ ppmtool-react-client/src/components/ProjectBoard/Backlog.js
@@ -7,6 +7,10 @@ class Backlog extends Component {
         const tasks = project_tasks_prop.map(project_task => (
             <ProjectTask key={project_task.id} project_task={project_task} />
         ));
+        let todoItems = tasks.filter(t => t.props.project_task.status === "TO_DO");
+        let inProgressItems = tasks.filter(t => t.props.project_task.status === "IN_PROGRESS");
+        let doneItems = tasks.filter(t => t.props.project_task.status === "DONE");
+
         return (
             <div className="container">
                 <div className="row">
@@ -15,14 +19,15 @@ class Backlog extends Component {
                             <div className="card-header bg-secondary text-white">
                                 <h3>TO DO</h3>
                             </div>
+                            {todoItems}
                         </div>
-                        {tasks}
                     </div>
                     <div className="col-md-4">
                         <div className="card text-center mb-2">
                             <div className="card-header bg-primary text-white">
                                 <h3>In Progress</h3>
                             </div>
+                            {inProgressItems}
                         </div>
                     </div>
                     <div className="col-md-4">
@@ -30,6 +35,7 @@ class Backlog extends Component {
                             <div className="card-header bg-success text-white">
                                 <h3>Done</h3>
                             </div>
+                            {doneItems}
                         </div>
                     </div>
                 </div>
