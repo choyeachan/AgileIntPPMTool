5bf8cccf9a43d2974cc173c3813745e6054db158
choyeachan <choyuichan@gmail.com>
2019-09-27 19:21:57 +0900

    branch62 - User 생성 및 등록시 에러 핸들링

---
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/exceptions/UsernameAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package io.agileintelligence.ppmtool.exceptions;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
