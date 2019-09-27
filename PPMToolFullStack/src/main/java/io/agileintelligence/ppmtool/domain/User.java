84a8e30517c7cb3e09d00f7139b8b7e67e020ce1
choyeachan <choyuichan@gmail.com>
2019-09-27 19:13:37 +0900

    branch61 - User 생성 및 등록 기능 구현

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/User.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/User.java
@@ -1,4 +1,132 @@
 package io.agileintelligence.ppmtool.domain;
 
-public class User {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Collection;
+import java.util.Date;
+
+@Entity
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "이름은 이메일 형식이여야 합니다")
+    @NotBlank(message = "이름은 필수값입니다")
+    @Column(unique = true)
+    private String username;
+    @NotBlank(message = "fullname을 작성하세요")
+    private String fullname;
+    @NotBlank(message = "비밀번호는 필수값입니다")
+    private String password;
+    @Transient
+    private String confirmPassword;
+    private Date created_At;
+    private Date updated_At;
+
+    //OneToMany with Project
+
+
+    public User(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public Date getCreated_At() {
+        return created_At;
+    }
+
+    public void setCreated_At(Date created_At) {
+        this.created_At = created_At;
+    }
+
+    public Date getUpdated_At() {
+        return updated_At;
+    }
+
+    public void setUpdated_At(Date updated_At) {
+        this.updated_At = updated_At;
+    }
+
+    @PrePersist
+    @JsonIgnore
+    protected void onCreate(){this.created_At = new Date();}
+
+    @PreUpdate
+    @JsonIgnore
+    protected void onUpdate(){this.updated_At = new Date();}
+
+    @Override
+    @JsonIgnore
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return true;
+    }
 }
